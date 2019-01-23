45e72b736c8dcd632eb603f4e28f94a1568993e6
k17rs091 <k17rs091@st.kyusan-u.ac.jp>
2019-01-23 13:27:09 +0900

    色を変換しました

--- www/keys.js
+++ www/keys.js
@@ -1,3 +1,3 @@
 // This is a JavaScript file
-var appKey = "";
-var clientKey = "";
+var appKey = "eb1cde364dfbea78bbfa8bd191b082d71625b2793b4834e81d2db6188fd70b35";
+var clientKey = "ed28dd8c69dd1922d26121c029bdbaeed86db58ef2f730befc8c9c0b15e5ee54";
